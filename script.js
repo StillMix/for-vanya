53c26a08e3153f708b6539699c718757f6b81573
StillMix <stillmixplaygames@gmail.com>
2022-12-20 23:46:15 +0300

    first commit

--- script.js
+++ script.js
@@ -21,85 +21,115 @@ function shuffle(numPool) {
 let a = shuffle(numPool);
 let arr = []
 function open() {
+
     arr = []
      issko.textContent = skoko + "/" + initialCards.length 
     document.querySelector('.button__container').innerHTML = ""
 
     div.classList.remove('disabled')
     btn.classList.add('disabled')
+    var otveto = [1,2,3,4,5,6]
+function otvetodd(otveto) {
+    for (var j, x, i = otveto.length; i; j = parseInt(Math.random() * i), x = otveto[--i], otveto[i] = otveto[j], otveto[j] = x);
+    return otveto;
+};
+let b = shuffle(otveto);
 
     random = a[0]
 
     a.splice(0, 1);
     initialCards.map((i) => {
+
         if (i.id == random) {
             text.textContent = i.text
-            if (i.one) {
-                let btn = document.createElement("button");
-                btn.className = "onel btn";
-                btn.innerHTML = i.one
-                btn.id = 1
-                btn.index = 1
-                btn.addEventListener('click', (event) => {
-                    otvet(event, i.otvet)
-                })
-                document.querySelector('.button__container').appendChild(btn);
+            b.map(b => {
+
+                if(b == 1){
+                    if (i.one) {
+                        let btn = document.createElement("button");
+                        btn.className = "onel btn";
+                        btn.innerHTML = i.one
+                        btn.id = 1
+                        btn.index = 1
+                        btn.addEventListener('click', (event) => {
+                            otvet(event, i.otvet)
+                        })
+                        document.querySelector('.button__container').appendChild(btn);
+                    }
+                 
+                }
+                if(b == 2){
+                if (i.two) {
+                    let btn = document.createElement("button");
+                    btn.className = "twol btn";
+                    btn.innerHTML = i.two
+                    btn.id = 2
+                    btn.index = 2
+                    btn.addEventListener('click', (event) => {
+                        otvet(event, i.otvet)
+                    })
+                    document.querySelector('.button__container').appendChild(btn);
+                }
+    
             }
-            if (i.two) {
-                let btn = document.createElement("button");
-                btn.className = "twol btn";
-                btn.innerHTML = i.two
-                btn.id = 2
-                btn.index = 2
-                btn.addEventListener('click', (event) => {
-                    otvet(event, i.otvet)
-                })
-                document.querySelector('.button__container').appendChild(btn);
+                if(b == 3){
+                if (i.three) {
+                    let btn = document.createElement("button");
+                    btn.className = "threel btn";
+                    btn.innerHTML = i.three
+                    btn.id = 3
+                    btn.index = 3
+                    btn.addEventListener('click', (event) => {
+                        otvet(event, i.otvet)
+                    })
+                    document.querySelector('.button__container').appendChild(btn);
+                }
+    
             }
-            if (i.three) {
-                let btn = document.createElement("button");
-                btn.className = "threel btn";
-                btn.innerHTML = i.three
-                btn.id = 3
-                btn.index = 3
-                btn.addEventListener('click', (event) => {
-                    otvet(event, i.otvet)
-                })
-                document.querySelector('.button__container').appendChild(btn);
+                if(b == 4){
+                if (i.four) {
+                    let btn = document.createElement("button");
+                    btn.className = "fourl btn";
+                    btn.innerHTML = i.four
+                    btn.id = 4
+                    btn.index = 4
+                    btn.addEventListener('click', (event) => {
+                        otvet(event, i.otvet)
+                    })
+                    document.querySelector('.button__container').appendChild(btn);
+                }
+    
             }
-            if (i.four) {
-                let btn = document.createElement("button");
-                btn.className = "fourl btn";
-                btn.innerHTML = i.four
-                btn.id = 4
-                btn.index = 4
-                btn.addEventListener('click', (event) => {
-                    otvet(event, i.otvet)
-                })
-                document.querySelector('.button__container').appendChild(btn);
+                if(b == 5){
+                if (i.five) {
+                    let btn = document.createElement("button");
+                    btn.className = "fivel btn";
+                    btn.innerHTML = i.five
+                    btn.id = 5
+                    btn.index = 5
+                    btn.addEventListener('click', (event) => {
+                        otvet(event, i.otvet)
+                    })
+                    document.querySelector('.button__container').appendChild(btn);
+                }
+    
             }
-            if (i.five) {
-                let btn = document.createElement("button");
-                btn.className = "fivel btn";
-                btn.innerHTML = i.five
-                btn.id = 5
-                btn.index = 5
-                btn.addEventListener('click', (event) => {
-                    otvet(event, i.otvet)
-                })
-                document.querySelector('.button__container').appendChild(btn);
+                if(b == 6){
+                if (i.six) {
+                    let btn = document.createElement("button");
+                    btn.className = "sixl btn";
+                    btn.innerHTML = i.six
+                    btn.id = 6
+                    btn.index = 6
+                    btn.addEventListener('click', (event) => {
+                        otvet(event, i.otvet)
+                    })
+                    document.querySelector('.button__container').appendChild(btn);
+                }
+      
             }
-            if (i.six) {
-                let btn = document.createElement("button");
-                btn.className = "sixl btn";
-                btn.innerHTML = i.six
-                btn.id = 6
-                btn.index = 6
-                btn.addEventListener('click', (event) => {
-                    otvet(event, i.otvet)
-                })
-                document.querySelector('.button__container').appendChild(btn);
-            }
+            })
+           
 
         }
 
