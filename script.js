6140cb458f87f7faa488bec96db18b75bd9d6d76
StillMix <stillmixplaygames@gmail.com>
2022-12-22 20:18:22 +0300

    first commit

--- script.js
+++ script.js
@@ -3,8 +3,16 @@ if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
 } else {
     document.getElementsByTagName('head')[0].innerHTML += '<link rel="stylesheet" href="style.css">'
 }
-
-document.querySelector('.start').addEventListener('click', (i) => {
+const start = document.querySelector('.btnstar')
+const exe = document.querySelector('.exe')
+start.addEventListener('click', (i) => {
     i.target.classList.add('disabled')
+    exe.classList.add('disabled')
       document.querySelector('.quiz-frame').classList.remove('disabled')
+})
+
+exe.addEventListener('click', (i) => {
+    i.target.classList.add('disabled')
+    start.classList.add('disabled')
+      document.querySelector('.exe-frame').classList.remove('disabled')
 })
