ecbb36b91638350281f309a04dcef7fbfa68393a
StillMix <stillmixplaygames@gmail.com>
2022-12-23 11:55:53 +0300

    first commit

--- exe/script.js
+++ exe/script.js
@@ -11,7 +11,7 @@ import {
 let ver = 0
 let never = 0
 let random = 0
-var numPool = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90]
+var numPool = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33]
 function shuffle(numPool) {
     for (var j, x, i = numPool.length; i; j = parseInt(Math.random() * i), x = numPool[--i], numPool[i] = numPool[j], numPool[j] = x);
     return numPool;
@@ -200,19 +200,19 @@ function vse() {
     const ochenka = document.querySelector('.ocenka')
     document.querySelector('.itog').classList.remove('disabled')
     div.classList.add('disabled')
-    if (ver < 45) {
+    if (ver < 15) {
         ochenka.textContent = '2'
         ochenka.classList.add('redl')
     }
-    if (ver > 45) {
+    if (ver > 15) {
         ochenka.textContent = '3'
         ochenka.classList.add('orangel')
     }
-    if (ver > 75) {
+    if (ver > 25) {
         ochenka.textContent = '4'
         ochenka.classList.add('postgreenl')
     }
-    if (ver > 84) {
+    if (ver > 29) {
         ochenka.textContent = '5'
         ochenka.classList.add('greenl')
     }
